a050202cf3adb482a0dcf37ada773e06c9e4f4e7
siwaredded <65319098+siwaredded@users.noreply.github.com>
2021-11-26 21:53:13 +0100

    Update ThreadAssignment.java

--- src/ThreadAssignment.java
+++ src/ThreadAssignment.java
@@ -2,7 +2,10 @@ public class ThreadAssignment {
 
     static class Counter {
         void count() {
-            //todo implement me
+            for ( int i = 350 ; i>=1 ; i-- ){
+                System.out.println(i);
+            }
+            System.out.println("FINISH !");
         }
     }
 
@@ -15,14 +18,26 @@ public class ThreadAssignment {
 
         @Override
         public void run() {
-            counter.count();
+            synchronized(counter) {
+                counter.count();
+            }
         }
     }
 
     public static void main(String[] args) {
         Counter counter = new Counter();
 
-        new MyThread(counter).start();
-        new MyThread(counter).start();
+        MyThread t1 = new MyThread(counter);
+        MyThread t2 = new MyThread(counter);
+        t1.start();
+        t2.start();
+        try {
+            t1.join();
+            t2.join();
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("DONE!");
     }
 }
